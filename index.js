6211f7457140fcf93cbfff10ac2bf2c52673644b
linyu <ethan_lamv@163.com>
2021-03-15 15:57:35 +0800

    upd

--- index.js
+++ index.js
@@ -1,52 +1,36 @@
 /**
  * 
- * @param {version} v1.0 
+ * @param {version} v2.0 
  */
 
-function vary(elm, attr, val) {
-  if (!elm.transform) {
-    elm.transform = {}
-  }
-  if (typeof val === 'undefined') {   // 取值
-    // 无值设置默认值
-    if (typeof elm.transform[attr] === 'undefined') {
-      switch (attr) {
-        case 'scale':
-          case 'scaleX':
-            case 'scaleY':
-              case 'scaleZ':
-                elm.transform[attr] = 1
-                break
-                default:
-          elm.transform[attr] = 0
-          
-        }
-        return elm.transform[attr]
-      } 
-    } else {      // 赋值
-    elm.transform[attr] = val
-    var transformVal = ''
-    for (const name in elm.transform) {
-      switch (name) {
-        case 'scale':
-        case 'scaleX':
-        case 'scaleY':
-        case 'scaleZ':
-          transformVal += name + '(' + (elm.transform[name]) + ')'
-          break
-        case 'rotate':
-        case 'rotateX':
-        case 'rotateY':
-        case 'rotateZ':
-        case 'skew':
-        case 'skewX':
-        case 'skewY':
-          transformVal += name + '(' + (elm.transform[name]) + 'deg)'
-          break
-          default:
-          transformVal += name + '(' + (elm.transform[name]) + 'px)'
-      }
+function vary(elm) {
+  var units = {},
+    transform,
+    transforms = {},
+    attrs = ['scale', 'scaleX', 'scaleY', 'scaleZ', 'rotate', 'rotateX', 'rotateY', 'rotateZ', 'skew', 'skewX', 'skewY', 'translate', 'translateX', 'translateY', 'translateZ']
+  // 
+  attrs.forEach(function (attr) {
+    if (attr.indexOf('scale') !== -1) {
+      units[attr] = ''
+      transforms[attr] = 1
+    } else {
+      units[attr] = attr.indexOf('translate') !== -1 ? 'px' : 'deg'
+      transforms[attr] = 0
     }
-    elm.style.WebkitTransform = elm.style.transform = transformVal
-  }
+    // 
+    Object.defineProperty(elm, attr, {
+      configurable: true,
+      get: function () {
+        return transforms[attr]
+      },
+      set: function (value) {
+        transform = ''
+        transforms[attr] = value
+        attrs.forEach(function (attr) {
+          transform += attr + '(' + transforms[attr] + units[attr] + ')'
+        })
+        elm.style.WebkitTransform = elm.style.transform = transform
+      }
+    })
+  })
 }
